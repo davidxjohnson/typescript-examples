d881c89b97cc1ba26fd76f99ed16eaaa018c73ba
david.x.johnson <johnson.david.x@gmail.com>
2022-11-12 19:53:32 +0000

    minor edits

--- k8s/pods/list-pods/src/list-pods.ts
+++ k8s/pods/list-pods/src/list-pods.ts
@@ -12,13 +12,13 @@ flag
   .addOption(new Option('-n --namespace <string>', 'The k8s namespace to use.').default('kube-system'))
   .addOption(new Option('-p --pagelimit <number>', 'The max number of items output per page.').default(10))
   .addOption(new Option('-t --timeout <number>', 'The max time to wait for a response.').default(10))
-  .addOption(new Option('-d --debug', 'Output one page of pod objects and exit.'))
+  .addOption(new Option('-d --dump', 'Output all content of pod objects.').default(false))
   .showHelpAfterError()
   .parse();
 const namespace: string = flag.opts()['namespace']
 const pagelimit: number = flag.opts()['pagelimit']
 const timeout: number = flag.opts()['timeout']
-const debug: boolean = flag.opts()['debug'] ? true : false
+const dump: boolean = flag.opts()['dump']
 
 // setup k8s to use current context (set prior to calling this program)
 const kubeConfig: k8s.KubeConfig = new k8s.KubeConfig();
@@ -27,30 +27,31 @@ const k8sApi: k8s.CoreV1Api = kubeConfig.makeApiClient(k8s.CoreV1Api)
 const cluster: string = String(kubeConfig.getCurrentCluster()?.name)
 
 // confirm input parameters (informational only)
-console.info("k8s cluster = %s\nnamespace = %s\npage limit = %s\ntimeout = %s\ndebug = %s", cluster, namespace, pagelimit, timeout, debug)
+console.info("k8s cluster = %s\nnamespace = %s\npage limit = %s\ntimeout = %s\ndump = %s", cluster, namespace, pagelimit, timeout, dump)
 
-// load container attributes
+// fetch container names (or other attributes) using pagination
 var nextToken: string | undefined = undefined
 do {
   await k8sApi.listNamespacedPod(namespace, undefined, undefined,
     nextToken, undefined, undefined, pagelimit,
     undefined, undefined, timeout, undefined, undefined)
     .then((res) => {
-      if (debug) { console.info(JSON.stringify(res.body.items, null, 2)); process.exit(0) }
-      var pod: k8s.V1Pod // makes type known to VSCode
-      for (pod of res.body.items) {
-        console.info(String(pod.metadata?.name))
+      if (dump) {
+        console.info(JSON.stringify(res.body?.items, null, 2))
+      } else {
+        for (var pod of res.body.items as k8s.V1Pod[]) {
+          console.info(String(pod.metadata?.name))
+        }
       }
       nextToken = res.body.metadata?._continue
-      k8s.V1Pod
     })
     .catch((error) => {
       if (error.hasOwnProperty('body')) { // api was reached, but has error
-        console.info("k8s api returned error:", error.body.message)
+        console.error("k8s api returned error:", error.body.message)
       } else {                            // api could not be reached.
-        console.info("k8s api not reachable:", error)
+        console.error("k8s api not reachable:", error)
       }
-      process.exit(1)
+      // process.exit(1)
     })
     .finally(() => {
       console.info("nextToken:", nextToken)
